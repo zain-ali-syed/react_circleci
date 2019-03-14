02adc59761e4e8307213504547a8d3189bb1013e
messagetobeats <chelnov63@hotmail.com>
2019-03-14 19:50:07 +0000

    first commit

---
+++ src/components/Users.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class Users extends Component {
+  state = {
+    users: []
+  };
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState(() => ({ users })));
+  }
+
+  render() {
+    const { users } = this.state;
+
+    if (users.length === 0) return <div data-testid="loading">Loading..</div>;
+    return (
+      <div>
+        <h3 data-testid="title">Users List</h3>
+        {users.map(user => (
+          <>
+            <div data-testid="first_name">{user.name}</div>
+            <div data-testid="email">{user.email}</div>
+            <hr />
+          </>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Users;
---
+++ src/components/Users.test.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { render, cleanup, waitForElement } from 'react-testing-library';
+
+import Users from './Users';
+
+global.fetch = require('jest-fetch-mock');
+
+const users = [
+  {
+    id: '123',
+    name: 'Rambo',
+    email: 'This@this.com'
+  },
+
+  {
+    id: '323',
+    name: 'Commando',
+    email: 'This@rmabo.com'
+  }
+];
+
+const user = users[0];
+
+afterEach(() => {
+  cleanup();
+});
+
+test('<Users /> when results load successfully', async () => {
+  fetch.mockResponseOnce(JSON.stringify(users));
+
+  const { getByTestId, queryByTestId } = render(<Users />);
+
+  expect(getByTestId('loading')).toBeTruthy();
+  await waitForElement(() => getByTestId('title'));
+  expect(queryByTestId('loading')).toBeFalsy();
+
+  expect(getByTestId('first_name').textContent).toBe(user.name);
+  expect(getByTestId('email').textContent).toBe(user.email);
+});
